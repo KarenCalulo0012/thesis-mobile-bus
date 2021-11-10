92a4f75f40778ff2aa41094feb987c54945564f3
basim <basim.usman94@gmail.com>
2021-11-11 04:42:20 +0500

    Passenger Report Work

--- app/src/main/java/com/example/transporte_pay/views/activity/PassengerReportActivity.java
+++ app/src/main/java/com/example/transporte_pay/views/activity/PassengerReportActivity.java
@@ -9,9 +9,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.example.transporte_pay.R;
 import com.example.transporte_pay.adapter.BookingAdapter;
+import com.example.transporte_pay.adapter.ConductorListAdapter;
 import com.example.transporte_pay.adapter.PassengerReportAdapter;
 import com.example.transporte_pay.data.api.ApiClient;
 import com.example.transporte_pay.data.model.Booking;
@@ -31,17 +33,26 @@ public class PassengerReport extends AppCompatActivity {
     DatePicker datePickerTo;
     Button search;
     SessionManager sessionManager;
-    String token,id, uFrom, uTo, uDate, dayPadded, monthPadded,dayPadded1, monthPadded1;
+    String token;
+    Integer id;
+    String uFrom;
+    String uTo;
+    String uDate;
+    String dayPadded;
+    String monthPadded;
+    String dayPadded1;
+    String monthPadded1;
     int uFromID, uToID;
     Integer roleId;
     List<Booking> bookings;
     PassengerReportAdapter passengerReportAdapter;
+    ConductorListAdapter conductorListAdapter;
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_report);
-        recyclerView = findViewById(R.id.transLog_rv);
+        recyclerView = findViewById(R.id.passengerTrans_rv);
 
         datePickerFrom = findViewById(R.id.editTextDateFrom);
         datePickerTo = findViewById(R.id.editTextDateTo);
@@ -51,8 +62,12 @@ public class PassengerReport extends AppCompatActivity {
 
         HashMap<String, String> user = sessionManager.getUSerDetails();
         token = user.get(SessionManager.PREF_USER_TOKEN);
-        id = user.get(SessionManager.ID);
 
+        HashMap<String, Integer> userDetail = sessionManager.getID();
+        id = userDetail.get(SessionManager.ID);
+        roleId = userDetail.get(SessionManager.ROLE);
+        passengerReportAdapter = new PassengerReportAdapter();
+        conductorListAdapter   = new ConductorListAdapter();
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,22 +97,62 @@ public class PassengerReport extends AppCompatActivity {
 
             }
         });
+        int day3 = datePickerFrom.getDayOfMonth();
+        int month3 = (datePickerFrom.getMonth() + 1);
+        int year3 = datePickerFrom.getYear();
+        int day4 = datePickerTo.getDayOfMonth();
+        int month4 = (datePickerTo.getMonth() + 1);
+        int year4 = datePickerTo.getYear();
+
+        monthPadded = String.format("%02d",month3 );
+        dayPadded = String.format("%02d",day3 );
+
+
+
+        uFrom = year3 + "-" + monthPadded + "-" + dayPadded;
+        Log.e("DATE", "********** DATE: " + uFrom);
+
+        monthPadded1 = String.format("%02d",month4 );
+        dayPadded1 = String.format("%02d",day4 );
+
+        uTo = year4 + "-" + monthPadded1 + "-" + dayPadded1;
+        Log.e("DATE", "********** DATE: " + uTo);
+        goToBookingList();
     }
 
+//    private void goToBookingList() {
+//        TransactionRequest transactionRequest = new TransactionRequest();
+//        transactionRequest.setFrom(uFrom);
+//        transactionRequest.setTo(uTo);
+//       transactionRequest.setUser_id(id);
+//        Call<TransactionResponse> callTransLog = ApiClient.getBusClient().getTransactionDataPassenger(transactionRequest, "Bearer " + token);
+//        callTransLog.enqueue(new Callback<TransactionResponse>() {
+//            @Override
+//            public void onResponse(Call<TransactionResponse> call, Response<TransactionResponse> response) {
+//                if (response.isSuccessful()){
+//                    bookings = response.body().getBookings();
+//                    passengerReportAdapter.setBookingList(bookings, roleId,uFrom,uTo);
+//                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//                    recyclerView.setAdapter(passengerReportAdapter);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<TransactionResponse> call, Throwable t) {
+//                Log.e("error", "busActivity:failed code=" + t.getMessage());
+//            }
+//        });
+//    }
     private void goToBookingList() {
-        TransactionRequest transactionRequest = new TransactionRequest();
-        transactionRequest.setFrom(uFrom);
-        transactionRequest.setTo(uTo);
-        transactionRequest.setUser_id(Integer.valueOf(id));
-        Call<TransactionResponse> callTransLog = ApiClient.getBusClient().getTransactionDataPassenger(transactionRequest, "Bearer " + token);
+        Call<TransactionResponse> callTransLog = ApiClient.getBusClient().getTransactionData( "Bearer " + token);
         callTransLog.enqueue(new Callback<TransactionResponse>() {
             @Override
             public void onResponse(Call<TransactionResponse> call, Response<TransactionResponse> response) {
                 if (response.isSuccessful()){
                     bookings = response.body().getBookings();
-                    passengerReportAdapter.setBookingList(bookings, roleId,uFrom,uTo);
+                    conductorListAdapter.setBookingList(bookings, roleId);
                     recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                    recyclerView.setAdapter(passengerReportAdapter);
+                    recyclerView.setAdapter(conductorListAdapter);
                 }
             }
 
